88eaa0efe2c8cdd0c3d6fc50fc775436354095a4
Huw Nolan <huw.nolan@imerja.com>
2009-08-23 16:17:44 +0100

    Prevent mysqldump from writing timestamp to dump files

--- misc/DumpSkel.bat
+++ misc/DumpSkel.bat
@@ -5,22 +5,22 @@ REM ---------------------------------------------------
 SET DB=cdb_dev
 SET DUMPFIL=D:\Dev\CDB\server\%DB%\sql\%DB%.skel.sql
 
-mysqldump -p%PWD% -u root %DB% --no-data --routines >%DUMPFIL%
-mysqldump -p%PWD% -u root %DB% m00_customers m06_datasources >>%DUMPFIL%
+mysqldump -p%PWD% -u root %DB% --no-data --routines --skip-dump-date >%DUMPFIL%
+mysqldump -p%PWD% -u root %DB% m00_customers m06_datasources --skip-dump-date >>%DUMPFIL%
 
 REM ---------------------------------------------------
 
 SET DB=cdc_rtg
 SET DUMPFIL=D:\Dev\CDB\client\%DB%\sql\%DB%.skel.sql
 
-mysqldump -p%PWD% -u root %DB% --no-data --routines >%DUMPFIL%
-mysqldump -p%PWD% -u root %DB% cdc_counters        >>%DUMPFIL%
+mysqldump -p%PWD% -u root %DB% --no-data --routines --skip-dump-date >%DUMPFIL%
+mysqldump -p%PWD% -u root %DB% cdc_counters        --skip-dump-date >>%DUMPFIL%
 
 REM ---------------------------------------------------
 
 SET DB=cdb_utils
 SET DUMPFIL=D:\Dev\CDB\misc\%DB%\sql\%DB%.skel.sql
 
-mysqldump -p%PWD% -u root %DB% --no-data --routines >%DUMPFIL%
+mysqldump -p%PWD% -u root %DB% --no-data --routines --skip-dump-date >%DUMPFIL%
 
 pause
