6d3b5bc2fe6b944ce34a3921dfd80b1e5edc6a38
Huw Nolan <huw.nolan@imerja.com>
2009-08-24 14:45:51 +0100

    dding convproc.sh and updated dumpproc.sh to call it

--- misc/dumpproc.sh
+++ misc/dumpproc.sh
@@ -8,5 +8,7 @@ mysql -p$PWD -u root -e "call cdb_utils.dump_code()"
 
 # move $DUMPDIR/cdb_dev.*.sql   $CWD/server/cdb_dev/procs
 # move $DUMPDIR/cdc_rtg.*.sql   $CWD/client/cdc_rtg/procs
-mv $DUMPDIR/cdb_utils.*.sql $CWD/misc/cdb_utils/procs
+# mv $DUMPDIR/cdb_utils.*.sql $CWD/misc/cdb_utils/procs
 
+# Combine dumped fragments and store under parent DB
+$CWD/misc/convproc.sh
