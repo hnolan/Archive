9363f4a1b1a0f0ae66e068f1ce89a775075b9e7e
Huw Nolan <huw.nolan@imerja.com>
2009-09-10 16:39:54 +0100

    Add nag_daily.sh as an executable

---
+++ client/bin/nag_daily.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Get global variables for this system
+#
+. $HOME/.imerja_env
+
+# Define local variables
+#
+BINDIR=$HOME/apps/nagios/bin
+EXPDIR=$HOME/export/events
+
+# Run daily routines to export data
+#
+/usr/bin/ruby ${BINDIR}/nag_export_events.rb
+
+# Send export files to central server
+#
+rsync -rzv --remove-source-files --include=${PREFIX}\*.csv --exclude \* ${EXPDIR}/ sm1::events
+
+# That's it
+
