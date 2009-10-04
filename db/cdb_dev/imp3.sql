15d2c05a22ee376fe1646fc940785636d3b1aec9
Huw Nolan <huw.nolan@imerja.com>
2009-10-04 16:53:33 +0100

    Delete unwanted sql files

--- db/cdb_dev/imp3.sql
+++
@@ -1,6 +0,0 @@
-truncate table tempds;
-load data infile 'd:/projects/cdb/cdc_rtg/export/LHC.lancsman.20090731.0916.meta' into table tempds;
-
-truncate table tempdt;
-load data infile 'd:/projects/cdb/cdc_rtg/export/LHC.lancsman.20090731.0916.data' into table tempdt;
-
